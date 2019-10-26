4b8cd0f0654778f05be4dcbf141d0a0e2cb7a815
Lara <lara.a.hercules@gmail.com>
2019-10-26 00:12:56 -0400

    Add Clock component and style it some.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React from 'react';
 import './App.css';
 import Dog from './Dog/Dog';
 import Meal from './Meal/Meal';
+import Clock from 'react-live-clock';
 
 function App() {
   return (
     <div className="App">
-        <div className="Profile">
-            <Dog dogName = "Zoey" />
-            <Meal />
+        <div className="Header">
+          <Clock
+            format={"dddd, MMMM Do YYYY, h:mm:ss a"}
+            ticking={true}
+            timezone={'Eastern'}
+          />
         </div>
         <div className="Profile">
-            <Dog dogName = "Boota" />
-            <Meal />
+          <Dog dogName = "Zoey" />
+          <Meal />
+        </div>
+        <div className="Profile">
+          <Dog dogName = "Boota" />
+          <Meal />
         </div>
     </div>
   );
 }
+
+setInterval(App, 1000);
 
 export default App;
